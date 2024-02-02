1769f786ef59f7a72486f399490b478d545e3e2c
drimeisis <dovrim@gmail.com>
2024-02-02 00:20:25 -0800

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "access_key" {
+  description = "Access key ID"
+  type        = string
+  default     = "test"
+}
+
+variable "access_key" {
+  description = "Access key ID"
+  type        = string
+  default     = "test"
+}
